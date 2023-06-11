e31de3e4a49220883ff5aeab01077227f3c1d47d
felipe <felipe.a.obregon@gmail.com>
2023-06-11 14:06:45 +0000

    make defMap request

--- app/api/route.tsx
+++ app/api/route.tsx
@@ -1,21 +1,49 @@
 import { NextResponse } from 'next/server'
- 
+
 const { Configuration, OpenAIApi } = require("openai");
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+    apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
+// create a route that accepts text and returns a definition map
 
+function createPrompt(text: string) {
+
+    const prompt = `Define each word of the following: \
+    "${text}". \
+     Use the following format: WORD=DEF. One word and def per line`
+
+     return prompt
+    
+}
 export async function GET() {
-    const response = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: "Translate 저는 to english",
-        max_tokens: 7,
-        temperature: 0,
-      });
-      
-    const text = response.data.choices[0].text
+    let response;
+    try {
+        response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: createPrompt("The quick fox jumped over the cat"),
+            max_tokens: 200,
+            temperature: 0,
+        });
+    } catch (e) {
+        console.error(e)
+    }
 
-  return NextResponse.json(text)
+    //process WORD=DEF into an object
+    const responseText = response.data.choices[0].text
+
+    console.log(responseText)
+
+
+    const lines = responseText.split('\n\n')
+    const defMap = new Map<string, string>()
+
+    for (let i = 0; i < lines.length; i++) {
+        const [key, value] = lines[i].split('=')
+        defMap.set(key, value)
+    }
+    console.log(defMap)
+
+    return NextResponse.json(Object.fromEntries(defMap))
 }

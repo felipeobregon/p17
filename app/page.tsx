8b586e7e42859c7512908ee3e16da632009e4c2b
felipe <felipe.a.obregon@gmail.com>
2023-06-11 21:11:16 +0000

    make call to api to build defMap

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,33 @@
 'use client'
 
 import Text from '@/components/Text'
+import { useEffect, useState } from 'react'
 
 export default function Home() {
+
+  const text = "hello how are you"
+
+  const [defMap, setDefMap] = useState(new Map())
+
+  // make fetch to create defMap
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/api', {method: 'POST', body: text});
+        const jsonData = await response.json();
+        setDefMap(new Map(Object.entries(jsonData)));
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    }
+
+    fetchData()
+  }, [])
 
 
   return (
     <>
-      <Text text="Hello who are you"/>
+      <Text text={text} defMap={defMap}/>
     </>
   )
 }

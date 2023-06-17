ff0e39087e26306af6a5030dc8393d1d30607e03
felipe <felipe.a.obregon@gmail.com>
2023-06-17 00:42:52 +0000

    add generate-story route

---
+++ app/api/generate-story/route.tsx
@@ -0,0 +1,9 @@
+export async function POST() {
+    // create a new story
+
+    // you could accept parameters like the word limit.
+
+
+}
+
+// im gonna build a piano, a sytnh, a piano manufacturing machine

c7a775ddf21080e69f190b1673bb1ec969b8e975
felipe <felipe.a.obregon@gmail.com>
2023-06-17 18:08:47 -0400

    add break-words style

--- components/Text.tsx
+++ components/Text.tsx
@@ -9,11 +9,11 @@ export default function Text({ text, defMap, handleClick }: Props) {
     const words = text.split(' ').map((word, index) => {
         return <span key={index}
             className="rounded-lg hover:bg-blue-500 hover:text-white"
-            onClick={() => handleClick(word)}>{word}&nbsp;</span>
+            onClick={() => handleClick(word)}>{word}</span>
     })
 
     return (
-        <p className="text-4xl">
+        <p className="text-4xl break-words">
             {words}
         </p>
     )

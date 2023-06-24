b7e703b798801d41ae5873d1101203820fdda5a6
felipe <felipe.a.obregon@gmail.com>
2023-06-24 13:12:02 -0400

    add defaultStory function

--- lib/get-story.tsx
+++ lib/get-story.tsx
@@ -1,7 +1,16 @@
-export default async function fetchData() {
+async function defaultStory(word: string) {
     const res = await fetch('/api/default-story')
 
     const text = await res.json()
 
     return text
+}
+
+
+export default async function generateStory(prompt: string) {
+    const res = await fetch('/api/generate-story', {method: 'POST', body: prompt})
+
+    const data = await res.json()
+
+    return data.story
 }

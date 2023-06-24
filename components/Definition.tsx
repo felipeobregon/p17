a8cbcd233b806554bc704fa610289e0872a64b41
felipe <felipe.a.obregon@gmail.com>
2023-06-23 20:31:25 -0400

    add styles to Definition

--- components/Definition.tsx
+++ components/Definition.tsx
@@ -6,7 +6,7 @@ type Props = {
 export default function Definition({word, def}: Props) {
     return (
         <>
-            <p className="text-2xl"><span className="font-bold">{word}</span>: {def}</p>
+            <p className="text-2xl fixed top-0 right-0 border-4 border-gray-200 rounded-2xl p-4 mx-2"><span className="font-bold">{word}</span>: {def}</p>
         </>
     )
 }

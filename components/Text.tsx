8b08867a61640cbc3b5951b9bcebd486a7232ba4
felipe <felipe.a.obregon@gmail.com>
2023-06-25 15:19:20 +0000

    add loading card

--- components/Text.tsx
+++ components/Text.tsx
@@ -1,11 +1,16 @@
+import { Card } from "antd";
 
 type Props = {
     text: string;
+    loading: boolean;
     handleClick: (word: string) => void
 }
 
-export default function Text({ text, handleClick }: Props) {
-    if (text) {
+export default function Text({ text, loading, handleClick }: Props) {
+    if (loading) {
+        return <Card loading={true}></Card>
+
+    } else {
         const words = text.split(' ').map((word, index) => {
             return <span key={index}
                 className="rounded-lg hover:bg-blue-500 hover:text-white"
@@ -17,7 +22,5 @@ export default function Text({ text, handleClick }: Props) {
                 {words}
             </p>
         )
-    } else {
-        return <h1>Loading</h1>
     }
 }

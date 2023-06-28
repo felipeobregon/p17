72b62686c12277ba2cd0b3f90a2f70a72ba26c86
felipe <felipe.a.obregon@gmail.com>
2023-06-28 17:30:20 -0400

    add fetchDefinitions / fetchDefineList

--- lib/create-list.tsx
+++ lib/create-list.tsx
@@ -1,4 +1,15 @@
-export default async function fetchData(words: string[]) {
+
+
+export default async function fetchDefinition(text: string) {
+    const res = await fetch('/api/definitions', {method: 'POST', body: text})
+
+    const data = await res.json()
+
+    return new Map<string,string>(Object.entries(data))
+}
+
+
+export async function fetchDefineList(words: string[]) {
     const defMap = new Map<string, string>()
 
     await Promise.all(words.map(word =>

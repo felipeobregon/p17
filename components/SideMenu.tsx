87fafe63521bda585f484397549e15fb99c8a63e
felipe <felipe.a.obregon@gmail.com>
2023-06-28 17:29:50 -0400

    add bg color to SideMenu

--- components/SideMenu.tsx
+++ components/SideMenu.tsx
@@ -3,7 +3,7 @@ export default function SideMenu() {
 
     return (
         <>
-            <ul className="fixed border-r-2 px-10 h-screen border-ltop-0 left-0">
+            <ul className="fixed border-r-2 px-10 h-screen bg-white top-0 left-0">
                 {menuItems.map(x => <li className="rounded-lg hover:bg-gray-100 p-5">{x}</li>)}
             </ul>
         </>
